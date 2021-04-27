38cd6ce740e6e461d742738d69d360f42ceba3c8
owo0219 <83285973+owo0219@users.noreply.github.com>
2021-04-27 20:40:42 +0800

    攝氏華氏

---
+++ c&f.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c=128;
+    double f;
+    f=c*9;
+    f=f/5+32;
+    printf("Äá¤ó%d«× = ",c);
+    printf("µØ¤ó%f«×",f);
+    return 0;
+}
